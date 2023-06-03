36c0d49bb9eb0e1db8f148bd0b31758793ea374d
growingtree-code <tjsruddks12@gmail.com>
2023-06-03 21:05:00 +0900

    get method param check

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.get("/capital/:country", (req, res) => {
+  const { country } = req.params;
+  console.log(country);
+  if (country == "Korea") {
+    res.json({ Seroul: country });
+  } else if (country == "Nepal") {
+    res.json({ Katmandu: country });
+  } else if (country == "Germany") {
+    res.json({ Berlin: country });
+  } else if (country == "Spain") {
+    res.json({ Madrid: country });
+  } else {
+    res.sendDate();
+  }
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
